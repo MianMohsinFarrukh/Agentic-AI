3337da940014145b49719ad4f493060d5665e249
Mian Mohsin Farrukh <164783660+MianMohsinFarrukh@users.noreply.github.com>
2025-06-17 09:03:49 +0500

    Multiple Agents

---
+++ multiple_Agents/Multi-Agents/README.md
@@ -0,0 +1,25 @@
+repo Link = https://github.com/panaversity/learn-agentic-ai
+api Link = https://aistudio.google.com/
+
+
+
+              ****************** UV & Vir env
+
+
+
+1- uv init .       install uv pakge first .
+2- uv venv         add virtual environment .
+3- .venv\Scripts\activate   activate virtual env .
+4- uv run explore-uv        uv.lock file create .
+
+
+8- .env                     create .env file for api .
+9- GEMINI_API_KEY=your api key  //set .env file 
+
+
+
+                   ****************** FOR AGENTS 
+10- uv add openai-agents
+
+
+
---
+++ multiple_Agents/Multi-Agents/main.py
@@ -0,0 +1,56 @@
+import os
+from dotenv import load_dotenv
+from agents import AsyncOpenAI, OpenAIChatCompletionsModel, Agent, Runner
+from agents.run import RunConfig
+
+# Load the environment variables
+load_dotenv()
+gemini_api_key = os.getenv("GEMINI_API_KEY")
+
+if not gemini_api_key:
+    raise ValueError("GEMINI_API_KEY is not set. Please ensure it is defined in your .env file.")
+
+external_client = AsyncOpenAI(
+    api_key=gemini_api_key,
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
+)
+
+model = OpenAIChatCompletionsModel(
+    model="gemini-2.0-flash",
+    openai_client=external_client
+)
+
+config = RunConfig(
+    model=model,
+    model_provider=external_client,
+    tracing_disabled=True
+)
+
+backend_agent = Agent(
+    name="Backend Expert",
+    handoff_description="Specialist agent for Backend Development",
+    instructions="You are a backend development expert. Help users with backend topics."
+)
+
+frontend_agent = Agent(
+    name="Frontend Expert",
+    handoff_description="Specialist agent for frontend Development",
+    instructions="You are a frontend expert. Help users with frontend topics."
+)
+
+web_dev_agent = Agent(
+    name="Web Developer Agent",
+    instructions="Route the question to the appropriate agent.",
+    handoffs=[frontend_agent, backend_agent]
+)
+
+user_question = input("Enter your question: ")
+ 
+ 
+try:
+    result = Runner.run_sync(web_dev_agent, user_question, run_config=config)
+    print("Result:", result.final_output)
+except Exception as e:
+    print("Error:", e)
+
+
